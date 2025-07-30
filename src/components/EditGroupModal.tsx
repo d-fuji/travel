d38188656d16edc268517eb1164aef3ce3c2c10a
d-fuji <d0a5i16ki@gmail.com>
2025-07-30 19:03:15 +0900

    feat: 招待機能

--- src/components/EditGroupModal.tsx
+++ src/components/EditGroupModal.tsx
@@ -386,7 +386,7 @@ export default function EditGroupModal({
       )}
 
       {/* メインモーダル */}
-      <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center p-4 z-50" style={{ alignItems: 'flex-start', paddingTop: '15vh' }}>
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
         <div className="bg-white rounded-2xl w-full max-w-md max-h-[90vh] overflow-hidden">
           {/* ヘッダー */}
           <div className="p-6 border-b border-gray-200">
